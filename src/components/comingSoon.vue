9dbe4c6c722b05ee1d84ca607a377b45305eb77d
Reza <effati78@protonmail.com>
2021-10-10 13:38:29 +0330

    ComingSoon component added

---
+++ src/components/comingSoon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <a href="https://github.com/effati78/effati78.com" target="_blank" rel="noopener">github.com</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
